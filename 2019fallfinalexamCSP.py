bc2c7122498cbe9128d65a6459bbeb048ad7405f
skylar-85740 <55498203+skylar-85740@users.noreply.github.com>
2019-12-18 10:55:55 -0500

    Add files via upload

---
+++ 2019fallfinalexamCSP.py
@@ -0,0 +1,50 @@
+#2019-20 Fall Computer Science Principles Final Exam
+
+#Ms. Haubold
+
+
+#Name
+#Skylar Strieper
+#Date
+#12/18/19
+
+
+#### INSTRUCTIONS ####
+#Create an etch a sketch turtle game
+#The turtle should move with the arrow keys (up, down, left and right), and draw
+#Space should clear the screen
+#o and p should make the pen size bigger and smaller
+#u should pick the pen up or put the pen down
+#All code must be commented
+#BONUS
+#Add a feature to change colors
+#
+
+#import required libraries
+
+
+#create turtle
+import turtle as trtl
+sketch = trtl.Turtle()
+
+#movement functions
+
+
+
+
+
+#color/drawing functions
+sketch.pensize()
+sketch.turtlesize(1)
+sketch.color()
+
+#create screen
+
+#bind to keypresses
+
+
+#listen
+
+wn = trtl.Screen()
+wn.mainloop()
+#mainloop
